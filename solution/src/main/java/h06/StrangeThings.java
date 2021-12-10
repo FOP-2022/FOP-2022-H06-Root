f9ddf6f9f254084a64f69619dd4e4572fcd1cb0a
Alexander Staeding <alexanderstaeding@gmail.com>
2021-12-10 19:44:23 +0100

    Braces

--- solution/src/main/java/h06/StrangeThings.java
+++ solution/src/main/java/h06/StrangeThings.java
@@ -7,12 +7,13 @@ public class StrangeThings {
     }
 
     public static int computeStrangeValue2(int n) {
-        if (n <= 0)
+        if (n <= 0) {
             return 1;
-        else if (0 == n % 2)
+        } else if (0 == n % 2) {
             return 1 + computeStrangeValue2(n - 1);
-        else
+        } else {
             return 1 + computeStrangeValue2(n - 3);
+        }
     }
 
     public static int whatTheHellIsThat1(int m, int n) {
@@ -20,18 +21,20 @@ public class StrangeThings {
     }
 
     public static int whatTheHellIsThat2(int m, int n) {
-        if (m <= 0 || n <= 0)
+        if (m <= 0 || n <= 0) {
             return 0;
-        else if (m < n)
+        } else if (m < n) {
             return whatTheHellIsThat2(1 + m, n - m);
-        else
+        } else {
             return whatTheHellIsThat2(1 + n, m - n);
+        }
     }
 
     public static double[] transformArrayIteratively(double[] in, Traverser t) {
         double[] result = new double[in.length];
-        for (int i = t.getFirstIndex(in); 0 <= i && i < in.length; i = t.getNextIndex(i))
+        for (int i = t.getFirstIndex(in); 0 <= i && i < in.length; i = t.getNextIndex(i)) {
             result[i] = 3.14 + 2 * in[i];
+        }
         return result;
     }
 
@@ -42,8 +45,9 @@ public class StrangeThings {
     }
 
     private static void doTheRecursion(double[] in, double[] out, Traverser t, int i) {
-        if (i < 0 || i >= in.length)
+        if (i < 0 || i >= in.length) {
             return;
+        }
         out[i] = 3.14 + 2 * in[i];
         doTheRecursion(in, out, t, t.getNextIndex(i));
     }
