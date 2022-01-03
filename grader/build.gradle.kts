9d5201730ff7321fd2527022855505337e44c049
dustin <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>
2022-01-03 21:03:16 +0100

    add implementations

--- grader/build.gradle.kts
+++ grader/build.gradle.kts
@@ -3,6 +3,10 @@ repositories {
 }
 
 dependencies {
-  implementation("org.sourcegrade:jagr-grader-api:0.1.0-SNAPSHOT")
-  implementation(project(":solution"))
+    implementation("org.sourcegrade:jagr-grader-api:0.1.0-SNAPSHOT")
+    implementation("org.mockito:mockito-inline:4.2.0")
+    implementation("org.powermock:powermock-api-mockito2:2.0.9")
+    implementation("org.powermock:powermock-module-junit4:2.0.9")
+    implementation("com.google.guava:guava:31.0.1-jre")
+    implementation(project(":solution"))
 }
